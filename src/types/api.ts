a1874872f8303120fde9d4b95a961e8b8a759975
viniAmorim <amorimm.viniciuss@gmail.com>
2021-08-23 18:31:22 -0300

    Making SectionTech Dynamic

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,3 +1,11 @@
+export type TechIcon = {
+  title: string
+  icon: {
+    url: string
+  }
+}
+
+
 export type LogoProps = {
   alternativeText: string
   url: string
@@ -25,9 +33,15 @@ export type SectionAboutProjectProps = {
   }
 }
 
+export type SectionTechProps =  {
+  title: string
+  techIcons: TechIcon[]
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
   sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
 }
 
