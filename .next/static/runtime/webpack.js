048d9c20cffe7536dbbe19fd05ac90a2ac5a85cf
viniAmorim <amorimm.viniciuss@gmail.com>
2021-08-23 16:30:40 -0300

    Making the header dynamic

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8522f34e2eb8fb35e7cd";
+/******/ 	var hotCurrentHash = "865315a8fac07ca1991d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
